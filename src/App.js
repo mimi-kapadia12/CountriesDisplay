692a904748afb9ebae4c439fb690651bd55a4e9f
mimi-kapadia12 <mansikapadia1999@gmail.com>
2024-02-11 11:02:14 +0530

    Initial Commit for displaying Countries

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
+export default function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((err) => console.log("error while fetching the data ", err));
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Countries</h1>
+      <div className="container">
+        <div className="row">
+          {countries.map((country) => (
+            <div
+              className="p-1 col-4 col-md-3 col-lg-2"
+              key={country.name.common}
+            >
+              <div className="card">
+                <img
+                  src={country.flags.png}
+                  className="card-img-top"
+                  alt={country.name.common}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{country.name.common}</h5>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
